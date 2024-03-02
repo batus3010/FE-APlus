910e6219832d979a9c48a830aa129ed93e6e5c7b
Công Thoại <dcthoai1023@gmail.com>
2024-03-02 21:45:11 +0700

    test

--- assets/js/exam.js
+++ assets/js/exam.js
@@ -1,38 +1,38 @@
-const urlParams = new URLSearchParams(window.location.search);
-const examId = urlParams.get("id");
-const url = './assets/js/topic.json';
-const examContainer = document.getElementById('exam-container');
+// const urlParams = new URLSearchParams(window.location.search);
+// const examId = urlParams.get("id");
+// const url = './assets/js/topic.json';
+// const examContainer = document.getElementById('exam-container');
 
-fetch(url)
-    .then(function (response) {
-        if (response.status === 200) {
-            return response.json();
-        } else {
-            throw new Error('Lỗi tải file JSON');
-        }
-    })
-    .then(function (data) {
-        for (const topic of data) {
-            if(topic.id == examId){
-                loadExam(topic);
-                break;
-            }
-        }
-    })
-    .catch(function (error) {
-        console.log(error);
-    });
+// fetch(url)
+//     .then(function (response) {
+//         if (response.status === 200) {
+//             return response.json();
+//         } else {
+//             throw new Error('Lỗi tải file JSON');
+//         }
+//     })
+//     .then(function (data) {
+//         for (const topic of data) {
+//             if(topic.id == examId){
+//                 loadExam(topic);
+//                 break;
+//             }
+//         }
+//     })
+//     .catch(function (error) {
+//         console.log(error);
+//     });
 
-function loadExam(topic){
-    examContainer.innerHTML = 
-            `<div class="topic">
-                <div class="topic__link" onclick="getExam(${topic.id})">
-                    <h3 class="title">${topic.title}</h3>
+// function loadExam(topic){
+//     examContainer.innerHTML = 
+//             `<div class="topic">
+//                 <div class="topic__link" onclick="getExam(${topic.id})">
+//                     <h3 class="title">${topic.title}</h3>
                     
-                    <div class="duration">Thời gian: <span>${topic.time}</span></div>
-                    <div class="deadline">Đến hạn vào: <span>${topic.deadline}</span></div>
+//                     <div class="duration">Thời gian: <span>${topic.time}</span></div>
+//                     <div class="deadline">Đến hạn vào: <span>${topic.deadline}</span></div>
 
-                    <div class="status">${topic.status}</div>
-                </div>
-            </div>`;
-}
+//                     <div class="status">${topic.status}</div>
+//                 </div>
+//             </div>`;
+// }
