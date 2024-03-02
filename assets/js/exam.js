1ab7c3f080f36b1fd41ced1985d66ed8f2464aed
Công Thoại <dcthoai1023@gmail.com>
2024-03-02 21:59:07 +0700

    fix bug page not found when get examId

--- assets/js/exam.js
+++ assets/js/exam.js
@@ -1,38 +1,49 @@
-// const urlParams = new URLSearchParams(window.location.search);
-// const examId = urlParams.get("id");
-// const url = './assets/js/topic.json';
-// const examContainer = document.getElementById('exam-container');
 
-// fetch(url)
-//     .then(function (response) {
-//         if (response.status === 200) {
-//             return response.json();
-//         } else {
-//             throw new Error('Lỗi tải file JSON');
-//         }
-//     })
-//     .then(function (data) {
-//         for (const topic of data) {
-//             if(topic.id == examId){
-//                 loadExam(topic);
-//                 break;
-//             }
-//         }
-//     })
-//     .catch(function (error) {
-//         console.log(error);
-//     });
+function getExamId(){
+    let examUrl = window.location.href;
+    let id = "";
 
-// function loadExam(topic){
-//     examContainer.innerHTML = 
-//             `<div class="topic">
-//                 <div class="topic__link" onclick="getExam(${topic.id})">
-//                     <h3 class="title">${topic.title}</h3>
+    for (let i = examUrl.indexOf("=") + 1; i < examUrl.length; i++) {
+        id += examUrl[i];
+    }
+
+    return id;
+}
+
+const url = './assets/js/topic.json';
+const examId = getExamId();
+const examContainer = document.getElementById('exam-container');
+
+fetch(url)
+    .then(function (response) {
+        if (response.status === 200) {
+            return response.json();
+        } else {
+            throw new Error('Lỗi tải file JSON');
+        }
+    })
+    .then(function (data) {
+        for (const topic of data) {
+            if(topic.id == examId){
+                loadExam(topic);
+                break;
+            }
+        }
+    })
+    .catch(function (error) {
+        console.log(error);
+    });
+
+function loadExam(topic){
+    examContainer.innerHTML = 
+            `<div class="topic">
+                <div class="topic__link" onclick="getExam(${topic.id})">
+                    <h3 class="title">${topic.title}</h3>
                     
-//                     <div class="duration">Thời gian: <span>${topic.time}</span></div>
-//                     <div class="deadline">Đến hạn vào: <span>${topic.deadline}</span></div>
+                    <div class="duration">Thời gian: <span>${topic.time}</span></div>
+                    <div class="deadline">Đến hạn vào: <span>${topic.deadline}</span></div>
 
-//                     <div class="status">${topic.status}</div>
-//                 </div>
-//             </div>`;
-// }
+                    <div class="status">${topic.status}</div>
+                </div>
+            </div>`;
+}
