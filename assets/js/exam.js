ad49fac4539f55d5ed41660be755234db69e07b0
Công Thoại <dcthoai1023@gmail.com>
2024-03-03 15:01:04 +0700

    add exampage

--- assets/js/exam.js
+++ assets/js/exam.js
@@ -35,15 +35,67 @@ fetch(url)
     });
 
 function loadExam(topic){
-    examContainer.innerHTML = 
-            `<div class="topic">
-                <div class="topic__link" onclick="getExam(${topic.id})">
-                    <h3 class="title">${topic.title}</h3>
-                    
-                    <div class="duration">Thời gian: <span>${topic.time}</span></div>
-                    <div class="deadline">Đến hạn vào: <span>${topic.deadline}</span></div>
+    let statusExam = '';
 
-                    <div class="status">${topic.status}</div>
+    if(topic.status == 'Đã hoàn thành'){
+        statusExam = 'complete';
+    } else if (topic.status == 'Chưa mở'){
+        statusExam = 'closed';
+    } else if (topic.status == 'Hết hạn'){
+        statusExam = 'failure';
+    }
+
+    examContainer.innerHTML = 
+        `<div class="topic" data-id="${topic.id}">
+            <h3 class="title">${topic.title}</h3>
+            
+            <div class="duration">Thời gian: <span>${topic.time}</span></div>
+            <div class="deadline">Đến hạn vào: <span>${topic.deadline}</span></div>
+
+            <div class="status ${statusExam}">${topic.status}</div>
+
+            <div class="button-box">
+                <button type="button" class="${statusExam}" id="show-answer">Xem đáp án</button>
+                <button type="button" class="${statusExam}" id="start-exam">Bắt đầu</button>
+            </div>
+        </div>`;
+
+    document.getElementById('start-exam').addEventListener('click', function(){
+        showExam(topic);
+    });
+}
+
+function showExam(topic){
+    let submitExam = document.getElementById('submit-button');
+    let examContentHtml = '';
+
+    topic.part.forEach(e => {
+        let questionsContentHtml = '';
+
+        if (e.type == 'checkbox'){
+            e.questions.forEach(question => {
+                questionsContentHtml += `
+
+                `;
+            });
+        } else if (e.type == 'text'){
+            e.questions.forEach(question => {
+                questionsContentHtml += `
+                
+                `;
+            });
+        }
+
+        examContentHtml += `
+            <div class="part">
+                <h3 class="part__title">${e.title}</h3>
+
+                <div class="part__list">
+                    ${questionsContentHtml}
                 </div>
-            </div>`;
+            </div>
+        `;
+    });
+
+    examContainer.innerHTML = examContentHtml + '<button id="submit-button">Submit</button>';
 }
